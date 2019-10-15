e6f1957d9f7cb0937436dcbe2249102c3d130063
dani <ikhdamuhammad@gmail.com>
2019-10-15 18:29:46 +0700

    Generating Code with Gii

--- views/country/index.php
+++ views/country/index.php
@@ -1,16 +1,40 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\LinkPager;
-?>
-<h1>Countries</h1>
-<ul>
-    <?php foreach ($countries as $country) : ?>
-        <li>
-            <?= Html::encode("{$country->code} ({$country->name})") ?>:
-            <?= $country->population ?>
-        </li>
-    <?php endforeach; ?>
-</ul>
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\CountrySearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
 
-<?= LinkPager::widget(['pagination' => $pagination]) ?>
+$this->title = 'Countries';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="country-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Country', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'code',
+            'name',
+            'population',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
